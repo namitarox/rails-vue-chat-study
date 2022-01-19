d1093666a408043bf07a4b2b9d8a1bedb52905ee
浪岡大貴 <namioka@pixel-hearts.co.jp>
2022-01-19 16:59:43 +0900

    update バリデーションとアソシエイトの設定

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :messages
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
