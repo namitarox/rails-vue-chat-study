560f725783cff97da729ab3d841534604eb7673f
浪岡大貴 <namioka@pixel-hearts.co.jp>
2022-01-19 17:58:28 +0900

    update ログイン時にメッセージを返すapiを作成

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,3 +1,8 @@
+3.times do |number|
+  Message.create(content: "#{number}番目のメッセージです！", user_id: User.first.id)
+  puts "#{number}番目のメッセージを作成しました"
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
